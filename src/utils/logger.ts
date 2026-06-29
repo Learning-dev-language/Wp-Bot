21436c6e3cfd73fe532c2193a258714ddbe724ce
Learning-dev-language <devilofparadiseerenyeager@gmail.com>
2026-06-29 11:55:18 +0500

    Add logger utility using pino for logging

---
+++ src/utils/logger.ts
@@ -0,0 +1,20 @@
+import pino from 'pino';
+import { config } from '../config';
+
+export const logger = pino({
+  level: config.logging.level,
+  transport: config.logging.pretty
+    ? {
+        target: 'pino-pretty',
+        options: {
+          colorize: true,
+          translateTime: 'SYS:standard',
+          ignore: 'pid,hostname',
+        },
+      }
+    : undefined,
+});
+
+export const createChildLogger = (module: string) => {
+  return logger.child({ module });
+};
